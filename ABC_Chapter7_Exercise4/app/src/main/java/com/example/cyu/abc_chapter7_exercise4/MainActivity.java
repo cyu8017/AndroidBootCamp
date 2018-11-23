5bf93c15ff72780827ec041160d92880b0e4fa69
Charlie Yu <cyu8017@gmail.com>
2018-11-22 21:32:10 -0500

    ABC_Chapter7_Exercise4
    Initial commit

---
+++ ABC_Chapter7_Exercise4/app/src/main/java/com/example/cyu/abc_chapter7_exercise4/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.cyu.abc_chapter7_exercise4;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Integer[] Car = {
+            R.drawable.car1,
+            R.drawable.car2,
+            R.drawable.car3,
+            R.drawable.car4,
+            R.drawable.car5,
+            R.drawable.car6
+    };
+
+    ImageView pic;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GridView grid = findViewById(R.id.gridView);
+        final ImageView pic = findViewById(R.id.imgLarge);
+        grid.setAdapter(new ImageAdapter(this));
+        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), "Selected Species " + (position + 1),
+                        Toast.LENGTH_SHORT).show();
+                pic.setImageResource(Car[position]);
+            }
+        });
+    }
+
+    public class ImageAdapter extends BaseAdapter {
+
+        private Context context;
+
+        public ImageAdapter(Context c) {
+            context = c;
+        }
+
+        @Override
+        public int getCount() {
+            return Car.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            pic = new ImageView(context);
+            pic.setImageResource(Car[position]);
+            pic.setScaleType(ImageView.ScaleType.FIT_XY);
+            pic.setLayoutParams(new GridView.LayoutParams(330, 330));
+            return pic;
+        }
+    }
+}
