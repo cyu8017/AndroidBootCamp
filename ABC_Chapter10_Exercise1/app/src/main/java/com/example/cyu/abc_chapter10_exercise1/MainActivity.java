79bb306aead80e12d32e7b0893f30f918b1eb518
Charlie Yu <cyu8017@gmail.com>
2018-11-23 18:54:08 -0500

    ABC_Chapter10_Example
    Initial commit

---
+++ ABC_Chapter10_Exercise1/app/src/main/java/com/example/cyu/abc_chapter10_exercise1/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.cyu.abc_chapter10_exercise1;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    AnimationDrawable lightsAnimation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView imgFrame = findViewById(R.id.imgActor);
+        imgFrame.setBackgroundResource(R.drawable.animation);
+        lightsAnimation = (AnimationDrawable) imgFrame.getBackground();
+
+        Button button1 = findViewById(R.id.btnStart);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lightsAnimation.start();
+            }
+        });
+    }
+}
