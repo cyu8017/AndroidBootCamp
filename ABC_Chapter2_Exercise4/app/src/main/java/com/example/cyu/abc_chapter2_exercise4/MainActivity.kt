c1e7ccda3e175015b78615777299cadc62235637
Charlie Yu <cyu8017@gmail.com>
2019-01-06 19:37:51 -0500

    Converted assignment from Java to Kotlin

---
+++ ABC_Chapter2_Exercise4/app/src/main/java/com/example/cyu/abc_chapter2_exercise4/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.cyu.abc_chapter2_exercise4
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.btnContactProfile)
+        button.setOnClickListener {
+            startActivity(Intent(this@MainActivity, Profile::class.java))
+        }
+    }
+}
