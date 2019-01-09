31dc2ab248c1a2880eddec21c0de8b06312474f6
Charlie Yu <cyu8017@gmail.com>
2019-01-08 21:10:15 -0500

    Updated Build Gradle

--- ABC_Chapter4_Exercise1/app/src/main/java/com/example/cyu/abc_chapter4_exercise1/MainActivity.kt
+++ ABC_Chapter4_Exercise1/app/src/main/java/com/example/cyu/abc_chapter4_exercise1/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.cyu.abc_chapter4_exercise1
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.RadioButton
