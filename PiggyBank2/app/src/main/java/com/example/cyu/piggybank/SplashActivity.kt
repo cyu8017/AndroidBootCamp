4eafb07ff6b4c26273ae7943c408eda22fe1ac28
Charlie Yu <cyu8017@gmail.com>
2019-02-08 22:03:57 -0500

    Convert assignment from Java to Kotlin

--- PiggyBank2/app/src/main/java/com/example/cyu/piggybank/SplashActivity.kt
+++ PiggyBank2/app/src/main/java/com/example/cyu/piggybank/SplashActivity.kt
@@ -4,36 +4,35 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
 
-    var isStarted = false
-    var progressStatus = 0
-    var handler: Handler? = null
+    private var mDelayHandler: Handler? = null
+    private val splashDelay: Long = 3000
+
+    private val mRunnable: Runnable = Runnable {
+        if (!isFinishing) {
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-        }, 3000)
+        // Initialize the Handler
+        mDelayHandler = Handler()
 
-        handler = Handler(Handler.Callback {
-            if (isStarted) {
-                progressStatus++
-            }
-            progressBarHorizontal.progress = progressStatus
-            handler?.sendEmptyMessageDelayed(0, 100)
-            true
-        })
+        // Navigate with delay
+        mDelayHandler!!.postDelayed(mRunnable, splashDelay)
+    }
 
-        handler?.sendEmptyMessage(0)
-
-
-        //https://www.androidly.net/327/android-progressbar-using-kotlin
-        // Verizon Corporate Dispute
+    public override fun onDestroy() {
+        if (mDelayHandler != null) {
+            mDelayHandler!!.removeCallbacks(mRunnable)
+        }
+        super.onDestroy()
     }
 }
